45876502504da50f1faf4bcdf00240c669601997
Victoria Probst <victoria.probst13@gmail.com>
2022-07-07 12:19:28 -0400

    Completed the Functionality to get By ID

--- src/main/java/com/dcc/videoGameApi/controllers/VideoGameController.java
+++ src/main/java/com/dcc/videoGameApi/controllers/VideoGameController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.persistence.criteria.CriteriaBuilder;
 import java.util.List;
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -25,6 +26,10 @@ public class VideoGameController {
     @GetMapping("/all")
     public List <VideoGame> getGames(){
         return service.getGames();
-}
+    }
+    @GetMapping("/getById/{id}")
+    public VideoGame getVideoGameById(@PathVariable Integer id){
+        return service.getVideoGameById(id);
+    }
 
     }
